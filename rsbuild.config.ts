242755eeef90dcb7f7bf5264889f22dff5eb7d55
aiibe <souksavanh.sy@gmail.com>
2025-05-17 16:55:38 +0200

    Display app version

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,8 +1,17 @@
 import path from "path";
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
+import { readFileSync } from "fs";
+
+// Extract version from package.json
+const { version } = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 export default defineConfig({
+  source: {
+    define: {
+      "process.env.APP_VERSION": JSON.stringify(version),
+    },
+  },
   plugins: [pluginReact()],
   resolve: {
     alias: {
