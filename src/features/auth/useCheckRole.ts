925b878d6150075c57f55dbd08581822ca94b132
Souk <souksavanh.sy@gmail.com>
2025-05-19 13:50:57 +0200

    Pages tabs navigation (#5)

--- src/features/auth/useCheckRole.ts
+++ src/features/auth/useCheckRole.ts
@@ -1,7 +1,13 @@
 import { pb } from "@/lib/db/pocketbase";
 import useAuthStore from "./useAuthStore";
 
-export function useCheckRole(role: "_superusers" | "agents" | "customers") {
+export enum Roles {
+  Admins = "_superusers",
+  Agents = "agents",
+  Customers = "customers",
+}
+
+export function useCheckRole(role: Roles) {
   const isAuth = useAuthStore((state) => state.auth);
   if (!pb.authStore?.record) return false;
   return role === pb.authStore.record.collectionName && isAuth;
---
+++ src/layouts/AuthLayout.tsx
@@ -0,0 +1,16 @@
+import { PropsWithChildren } from "react";
+import { NavigationTabs } from "@/features/navigation/NavigationTabs";
+
+export default function AuthLayout(props: PropsWithChildren) {
+  const { children } = props;
+
+  return (
+    <section className="grid w-full grid-cols-12 gap-2">
+      <div className="col-span-10 col-start-2 py-4 md:col-span-6 md:col-start-4">
+        <NavigationTabs />
+
+        {children}
+      </div>
+    </section>
+  );
+}
